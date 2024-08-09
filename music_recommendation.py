352045b1d3a772677506fcc88d9aece64d1b6e74
Balakrishna <127603062+balu2257@users.noreply.github.com>
2024-08-09 11:18:11 +0530

    Add files via upload

--- music_recommendation.py
+++ music_recommendation.py
@@ -9,6 +9,7 @@ Original file is located at
 
 import pandas as pd
 import numpy as np
+import sklearn
 
 from sklearn.feature_extraction.text import TfidfVectorizer
 from sklearn.metrics.pairwise import cosine_similarity
